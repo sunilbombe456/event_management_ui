ca3b6c2ec3858d60116deafed08acf933fcad946
Sunil Bombe <sunilbombe456@gmail.com>
2020-11-10 12:46:50 -0800

    Added Auth Guard

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EventManagementApiService } from '../../services/event-management-api.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private eventManagementApiService: EventManagementApiService) { }
 
   ngOnInit(): void {
   }
+
+  loggedIn = this.eventManagementApiService.isLoggedIn;
+
+
 
 }
